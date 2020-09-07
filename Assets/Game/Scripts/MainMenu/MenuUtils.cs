d97bc718976e89ef48d8a8628c3855ca8c3400df
Sir Code <taylorschneider07@yahoo.com>
2020-09-06 23:17:41 -0400

    Initial Update

---
+++ Assets/Game/Scripts/MainMenu/MenuUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUtils : MonoBehaviour
+{
+    public GameObject fadePanel;
+    public float waitTime;
+
+    private Animator fadeAnimator;
+
+    private void Start() {
+        fadeAnimator = fadePanel.GetComponent<Animator>();
+    }
+
+    private void Play() {
+        StartCoroutine(SceneTransition("SnakeGame"));
+    }
+
+    private void Quit() {
+        Application.Quit();
+    }
+
+    IEnumerator SceneTransition(string scene) {
+        fadeAnimator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(waitTime);
+        // SceneManager.LoadScene(scene);
+    }
+}
