4182b4a6ffbb63debaa5d60234314516ca4c9cfa
Sir Code <taylorschneider07@yahoo.com>
2020-09-07 22:56:30 -0400

    TOO MANY THINGS

---
+++ Assets/Game/Scripts/MenuUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUtils : MonoBehaviour
+{
+    public float transitionTime;
+    public Animator fadeAnimator;
+    public string trigger;
+
+    public void Switch(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+    public void Open(string url) {
+        Application.OpenURL(url);
+    }
+
+    public void TransitionScene(string scene) {
+        StartCoroutine(SceneTransition(scene));
+    }
+
+    private IEnumerator SceneTransition(string scene) {
+        fadeAnimator.SetTrigger(trigger);
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scene);
+    }
+}
